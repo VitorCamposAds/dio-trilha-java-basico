b2c952018ba6b4a0620ac004e7fb0b2555455db0
VitorCamposAds <vitorbeatle@gmail.com>
2024-05-09 00:40:49 -0300

    Teste

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,20 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(13);
+        System.out.println("Volume atual: " + smartTv.canal);
+
+        System.out.println("Tv Ligada:" + smartTv.ligada);
+        System.out.println("Canal Atual:" + smartTv.canal);
+        System.out.println("Volume Atual?:" + smartTv.volume);
+
+
+    }
+}
